66a9848b04d3818e003057a123fc5dd91aa1d4c6
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-05-06 17:40:39 +0300

    add getWeatherByCoordinates function to call in the test cases

--- src/main/kotlin/domain/usecase/GetWeatherByLongitudeAndLatitudeUseCase.kt
+++ src/main/kotlin/domain/usecase/GetWeatherByLongitudeAndLatitudeUseCase.kt
@@ -1,8 +1,13 @@
 package org.beijingteam.domain.usecase
 
+import org.beijingteam.domain.entity.Weather
 import org.beijingteam.domain.repository.WeatherRepository
 
 class GetWeatherByLongitudeAndLatitudeUseCase(
     private val weatherRepository: WeatherRepository
 ) {
+
+    suspend fun getWeatherByCoordinates(longitude: Double, latitude: Double): Result<Weather> {
+       TODO()
+    }
 }
