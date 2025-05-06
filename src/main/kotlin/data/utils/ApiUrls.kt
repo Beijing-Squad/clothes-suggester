5f4cf532f214ded318ee0261e54c205566d0b06e
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2025-05-06 19:06:52 +0300

    create API URLs for a central utils file

---
+++ src/main/kotlin/data/utils/ApiUrls.kt
@@ -0,0 +1,6 @@
+package org.beijingteam.data.utils
+
+object ApiUrls {
+    const val WEATHER_URL = "https://api.open-meteo.com/v1/forecast"
+    const val GEOCODING_URL = "https://geocoding-api.open-meteo.com/v1/search"
+}
