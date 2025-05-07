9d675325669d7205084248700d66f4f6b08edb3d
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2025-05-07 15:15:35 +0300

    refactor: merge enums into entity package and clean usage

--- src/main/kotlin/domain/entity/Weather.kt
+++ src/main/kotlin/domain/entity/Weather.kt
@@ -1,12 +1,7 @@
 package org.beijingteam.domain.entity
 
-import org.beijingteam.domain.entity.enums.TemperatureCategory
-import org.beijingteam.domain.entity.enums.WeatherCondition
-
-
 data class Weather(
     val temperature: Double,
-    val relativeHumidity: Int,
     val weatherCondition: WeatherCondition,
     val temperatureCategory: TemperatureCategory
 )
