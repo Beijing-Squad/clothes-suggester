b1f7c0f51deb959a39b5c5e11ff4bd61c36b24c8
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2025-05-07 15:17:50 +0300

    scaffold WeatherRepositoryImpl with dependencies

---
+++ src/main/kotlin/data/weather/repository/WeatherRepositoryImpl.kt
@@ -0,0 +1,17 @@
+package org.beijingteam.data.weather.repository
+
+import domain.entity.LocationCoordinate
+import org.beijingteam.data.weather.mapper.WeatherMapper
+import org.beijingteam.data.weather.datasource.WeatherRemoteDataSource
+import org.beijingteam.domain.entity.Weather
+import org.beijingteam.domain.repository.WeatherRepository
+
+class WeatherRepositoryImpl(
+    private val remoteDataSource: WeatherRemoteDataSource,
+    private val weatherMapper: WeatherMapper
+) : WeatherRepository {
+    override suspend fun getWeatherByCoordinate(locationCoordinate: LocationCoordinate): Weather {
+        TODO("Not yet implemented")
+    }
+
+}
