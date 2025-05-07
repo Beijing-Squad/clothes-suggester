f49f121f0e4b54f4d158d84caee973c19785a1f9
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2025-05-07 12:50:15 +0300

    use only one url

--- src/main/kotlin/data/utils/ApiUrls.kt
+++ src/main/kotlin/data/utils/ApiUrls.kt
@@ -2,5 +2,4 @@ package org.beijingteam.data.utils
 
 object ApiUrls {
     const val WEATHER_URL = "https://api.open-meteo.com/v1/forecast"
-    const val GEOCODING_URL = "https://geocoding-api.open-meteo.com/v1/search"
 }
