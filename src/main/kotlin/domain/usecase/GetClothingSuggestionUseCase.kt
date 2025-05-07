986535aca79dc57bbb1a66664b445a71d7972598
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2025-05-07 13:01:02 +0300

    create empty use case for clothes suggestion

---
+++ src/main/kotlin/domain/usecase/GetClothingSuggestionUseCase.kt
@@ -0,0 +1,8 @@
+package org.beijingteam.domain.useCase
+
+import domain.repository.ClothesRepository
+
+class GetClothingSuggestionUseCase(private val clothesRepository: ClothesRepository) {
+    //TODO: Implement the logic to get clothing suggestions based on weather conditions and temperature
+
+}
