3975615f1820f2738b2c63e5597cfbe46de6ad5b
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-05-07 17:53:08 +0300

    refactor the test cases to accommodate the new changes made to dev and use case

--- src/test/kotlin/domain/usecase/GetWeatherByLongitudeAndLatitudeUseCaseTest.kt
+++ src/test/kotlin/domain/usecase/GetWeatherByLongitudeAndLatitudeUseCaseTest.kt
@@ -1,16 +1,18 @@
 package domain.usecase
 
 import com.google.common.truth.Truth.assertThat
+import domain.entity.LocationCoordinate
 import helper.createWeather
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
-import org.beijingteam.domain.entity.enums.TemperatureCategory
-import org.beijingteam.domain.entity.enums.WeatherCondition
+import org.beijingteam.domain.entity.TemperatureCategory
+import org.beijingteam.domain.entity.WeatherCondition
 import org.beijingteam.domain.repository.WeatherRepository
 import domain.useCase.GetWeatherByLongitudeAndLatitudeUseCase
 import kotlin.test.BeforeTest
 import kotlin.test.Test
+import kotlin.test.assertFailsWith
 
 class GetWeatherByLongitudeAndLatitudeUseCaseTest {
 
@@ -26,42 +28,38 @@ class GetWeatherByLongitudeAndLatitudeUseCaseTest {
     @Test
     fun `should return result with weather when repository returns the weather successfully`() = runTest {
         // Given
-        val longitude = 116.397
-        val latitude = 39.904
+        val locationCoordinate = LocationCoordinate(latitude = 39.904, longitude = 116.397)
         val expectedWeather = createWeather(
             temperature = 20.0,
-            relativeHumidity = 50.0,
             weatherCondition = WeatherCondition.CLEAR,
             temperatureCategory = TemperatureCategory.MILD
         )
-        coEvery { weatherRepository.getWeatherByLongitudeAndLatitude(longitude, latitude) } returns Result.success(
-            expectedWeather
-        )
+        coEvery { weatherRepository.getWeatherByCoordinate(locationCoordinate) } returns
+                expectedWeather
+
 
         // When
-        val result = getWeatherByLongitudeAndLatitudeUseCase.getWeatherByCoordinates(longitude, latitude)
+        val result = getWeatherByLongitudeAndLatitudeUseCase.getWeatherByCoordinates(locationCoordinate)
 
         // Then
-        assertThat(result).isEqualTo(Result.success(expectedWeather))
+        assertThat(result).isEqualTo(expectedWeather)
     }
 
     @Test
     fun `should return weather when called with maximum latitude`() = runTest {
         // Given
-        val longitude = 0.0
-        val latitude = 90.0
+        val locationCoordinate = LocationCoordinate(latitude = 90.0, longitude = 0.0)
         val expectedWeather = createWeather(
             temperature = 20.0,
-            relativeHumidity = 50.0,
             weatherCondition = WeatherCondition.CLEAR,
             temperatureCategory = TemperatureCategory.MILD
         )
-        coEvery { weatherRepository.getWeatherByLongitudeAndLatitude(longitude, latitude) } returns Result.success(
-            expectedWeather
-        )
+        coEvery { weatherRepository.getWeatherByCoordinate(locationCoordinate) } returns
+                expectedWeather
+
 
         // When
-        val result = getWeatherByLongitudeAndLatitudeUseCase.getWeatherByCoordinates(longitude, latitude)
+        val result = getWeatherByLongitudeAndLatitudeUseCase.getWeatherByCoordinates(locationCoordinate)
 
         // Then
         assertThat(result).isEqualTo(Result.success(expectedWeather))
@@ -70,93 +68,82 @@ class GetWeatherByLongitudeAndLatitudeUseCaseTest {
     @Test
     fun `should return weather when called with minimum latitude`() = runTest {
         // Given
-        val longitude = 0.0
-        val latitude = -90.0
+        val locationCoordinate = LocationCoordinate(latitude = 0.0, longitude = -90.0)
         val expectedWeather = createWeather(
             temperature = -15.0,
-            relativeHumidity = 65.0,
             weatherCondition = WeatherCondition.SNOW,
             temperatureCategory = TemperatureCategory.FREEZING
         )
 
-        coEvery { weatherRepository.getWeatherByLongitudeAndLatitude(longitude, latitude) } returns Result.success(
-            expectedWeather
-        )
+        coEvery { weatherRepository.getWeatherByCoordinate(locationCoordinate) } returns
+                expectedWeather
+
 
         // When
-        val result = getWeatherByLongitudeAndLatitudeUseCase.getWeatherByCoordinates(longitude, latitude)
+        val result = getWeatherByLongitudeAndLatitudeUseCase.getWeatherByCoordinates(locationCoordinate)
 
         // Then
-        assertThat(result).isEqualTo(Result.success(expectedWeather))
+        assertThat(result).isEqualTo(expectedWeather)
     }
 
     @Test
     fun `should return weather when called with maximum longitude`() = runTest {
         // Given
-        val longitude = 180.0
-        val latitude = 0.0
+        val locationCoordinate = LocationCoordinate(latitude = 0.0, longitude = 180.0)
         val expectedWeather = createWeather(
             temperature = 25.0,
-            relativeHumidity = 60.0,
             weatherCondition = WeatherCondition.PARTLY_CLOUDY,
             temperatureCategory = TemperatureCategory.WARM
         )
-        coEvery { weatherRepository.getWeatherByLongitudeAndLatitude(longitude, latitude) } returns Result.success(
-            expectedWeather
-        )
+        coEvery { weatherRepository.getWeatherByCoordinate(locationCoordinate) } returns
+                expectedWeather
+
 
         // When
-        val result = getWeatherByLongitudeAndLatitudeUseCase.getWeatherByCoordinates(longitude, latitude)
+        val result = getWeatherByLongitudeAndLatitudeUseCase.getWeatherByCoordinates(locationCoordinate)
 
         // Then
-        assertThat(result).isEqualTo(Result.success(expectedWeather))
+        assertThat(result).isEqualTo(expectedWeather)
     }
 
     @Test
     fun `should return weather when called with minimum longitude`() = runTest {
         // Given
-        val longitude = -180.0
-        val latitude = 0.0
+        val locationCoordinate = LocationCoordinate(latitude = 0.0, longitude = -180.0)
         val expectedWeather = createWeather(
             temperature = 25.0,
-            relativeHumidity = 60.0,
             weatherCondition = WeatherCondition.PARTLY_CLOUDY,
             temperatureCategory = TemperatureCategory.WARM
         )
-        coEvery { weatherRepository.getWeatherByLongitudeAndLatitude(longitude, latitude) } returns Result.success(
-            expectedWeather
-        )
+        coEvery { weatherRepository.getWeatherByCoordinate(locationCoordinate) } returns
+                expectedWeather
 
         // When
-        val result = getWeatherByLongitudeAndLatitudeUseCase.getWeatherByCoordinates(longitude, latitude)
+        val result = getWeatherByLongitudeAndLatitudeUseCase.getWeatherByCoordinates(locationCoordinate)
 
         // Then
-        assertThat(result).isEqualTo(Result.success(expectedWeather))
+        assertThat(result).isEqualTo(expectedWeather)
     }
 
     @Test
     fun `should return failure when latitude exceeds 90 `() = runTest {
         // Given
-        val longitude = 0.0
-        val latitude = 91.0
-
-        // When
-        val result = getWeatherByLongitudeAndLatitudeUseCase.getWeatherByCoordinates(longitude, latitude)
+        val locationCoordinate = LocationCoordinate(latitude = 91.0, longitude = 0.0)
 
         // Then
-        assertThat(result.isFailure).isTrue()
+        assertFailsWith<IllegalArgumentException> {
+            getWeatherByLongitudeAndLatitudeUseCase.getWeatherByCoordinates(locationCoordinate)
+        }
     }
 
     @Test
     fun `should return failure when longitude exceeds 180`() = runTest {
         // Given
-        val longitude = 190.0
-        val latitude = 0.0
-
-        // When
-        val result = getWeatherByLongitudeAndLatitudeUseCase.getWeatherByCoordinates(longitude, latitude)
+        val locationCoordinate = LocationCoordinate(latitude = 0.0, longitude = 190.0)
 
         // Then
-        assertThat(result.isFailure).isTrue()
+        assertFailsWith<IllegalArgumentException> {
+            getWeatherByLongitudeAndLatitudeUseCase.getWeatherByCoordinates(locationCoordinate)
+        }
     }
 }
