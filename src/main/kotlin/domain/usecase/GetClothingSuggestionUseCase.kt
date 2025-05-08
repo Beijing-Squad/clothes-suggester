94aed0163b17c7201a8036eeffff21b10af7ceb0
AdelTamer <adeltammer@gmail.com>
2025-05-08 13:18:03 +0300

    implement get clothing suggestion use case

--- src/main/kotlin/domain/usecase/GetClothingSuggestionUseCase.kt
+++ src/main/kotlin/domain/usecase/GetClothingSuggestionUseCase.kt
@@ -1,9 +1,13 @@
 package org.beijingteam.domain.useCase
 
+import domain.entity.Cloth
+import domain.entity.ClothType
 import domain.repository.ClothesRepository
 
 class GetClothingSuggestionUseCase(private val clothesRepository: ClothesRepository) {
 
-    //Implement the logic to get clothing suggestions based on weather conditions and temperature
+    fun getClothByType(clothType: ClothType): List<Cloth> {
+        return clothesRepository.getClothByType(clothType)
+    }
 
 }
