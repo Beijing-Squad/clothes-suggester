a97894f00ba9b45c573d00335157a72175c4216b
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2025-05-08 22:20:06 +0300

    refactor ClothesDataSourceImpl to use Map for O(1) cloth type

--- src/main/kotlin/data/local/clothes/datasource/ClothesDataSourceImpl.kt
+++ src/main/kotlin/data/local/clothes/datasource/ClothesDataSourceImpl.kt
@@ -5,7 +5,7 @@ import domain.entity.ClothType
 
 class ClothesDataSourceImpl: ClothesDataSource {
     override fun getClothesByType(clothType: ClothType): List<Clothes> {
-        return clothes.filter { it.clothType == clothType }
+        return filteredClothesByType[clothType] ?: emptyList()
     }
 
     @OptIn(kotlin.uuid.ExperimentalUuidApi::class)
@@ -34,5 +34,7 @@ class ClothesDataSourceImpl: ClothesDataSource {
             Clothes(clothName = "Padded Vest", clothType = ClothType.HEAVY_CLOTH),
             Clothes(clothName = "Winter Boots", clothType = ClothType.HEAVY_CLOTH),
         )
+
+        private val filteredClothesByType: Map<ClothType, List<Clothes>> = clothes.groupBy { it.clothType }
     }
 }
